7e81ef907406cb0523d75a7a86470173982add54
ZhelinCheng <chengzhelin@foxmail.com>
2022-03-25 17:27:02 +0800

    feat: ip获取方式修改为本地

--- src/utils/ipv6.ts
+++ src/utils/ipv6.ts
@@ -2,31 +2,25 @@
  * @Author       : 程哲林
  * @Date         : 2022-03-25 15:56:58
  * @LastEditors  : 程哲林
- * @LastEditTime : 2022-03-25 17:11:33
+ * @LastEditTime : 2022-03-25 17:26:03
  * @FilePath     : /ipv6-ddns/src/utils/ipv6.ts
  * @Description  : 未添加文件描述
  */
 
 import axios from 'axios'
 import { logger } from './logger'
+import { internalIpV6 } from 'internal-ip'
 
 let memo = ''
 
 export async function ipv6 () {
   try {
-    const res = await axios.get('https://test.ipw.cn/api/ip/myip?json')
-    const { status, data } = res
-
-    if (status < 300) {
-      const ip: string = data?.IP || ''
-      const IPVersion = data?.IPVersion || ''
-
-      if (IPVersion === 'IPv6' && memo !== ip) {
-        memo = ip;
-        return {
-          ip,
-          prefix: ip.split(":").slice(0, 4).join(':')
-        }
+    const ip = await internalIpV6()
+    if (ip && memo !== ip) {
+      memo = ip;
+      return {
+        ip,
+        prefix: ip.split(":").slice(0, 4).join(':')
       }
     }
   } catch(e) {

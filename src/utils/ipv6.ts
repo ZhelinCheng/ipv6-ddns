11b641d88c3074c0c523d36be1f07afb7e82901c
ZhelinCheng <chengzhelin@foxmail.com>
2022-03-25 17:32:18 +0800

    feat: 更新地址

--- src/utils/ipv6.ts
+++ src/utils/ipv6.ts
@@ -2,12 +2,11 @@
  * @Author       : 程哲林
  * @Date         : 2022-03-25 15:56:58
  * @LastEditors  : 程哲林
- * @LastEditTime : 2022-03-25 17:26:03
+ * @LastEditTime : 2022-03-25 17:32:12
  * @FilePath     : /ipv6-ddns/src/utils/ipv6.ts
  * @Description  : 未添加文件描述
  */
 
-import axios from 'axios'
 import { logger } from './logger'
 import { internalIpV6 } from 'internal-ip'
 

e57cee24680ccc4acbe816021bedb22ee9654486
ZhelinCheng <chengzhelin@foxmail.com>
2022-10-24 16:09:59 +0800

    feat: ipv6获取优化

--- src/utils/ipv6.ts
+++ src/utils/ipv6.ts
@@ -2,21 +2,28 @@
  * @Author       : 程哲林
  * @Date         : 2022-03-25 15:56:58
  * @LastEditors  : 程哲林
- * @LastEditTime : 2022-10-22 16:48:21
+ * @LastEditTime : 2022-10-24 16:09:21
  * @FilePath     : /ipv6-ddns/src/utils/ipv6.ts
  * @Description  : 未添加文件描述
  */
 
 import { logger } from './logger';
-import axios from 'axios';
+import { exec } from 'child_process'
 
 let memo = '';
 
 // https://v6.ipv6-test.com/api/myip.php
 
 const address = async (): Promise<string> => {
-  const { status, data } = await axios.get('https://v6.ipv6-test.com/api/myip.php')
-  return status === 200 ? data : ''
+  return new Promise((resolve, reject) => {
+    exec('curl 6.ipw.cn', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
 };
 
 export async function ipv6() {
